d530164149476c761f216a21ddcb7eb3c74c9df7
kameoxylon <kre3ed@gmail.com>
2016-08-10 23:53:48 -0400

    updated jquery, finished introHome

--- js/helper.js
+++ js/helper.js
@@ -1,35 +1,28 @@
-$(document).ready(function () {
-    var img_array = [0, 1],
+/*$(document).ready(function () {
+    var img_array = [0, 1, 2, 3, 4],
         newIndex = 0,
         index = 0,
-        interval = 5000;
+        interval = 6000;
     (function changeBg() {
 
-        //  --------------------------
-        //  For random image rotation:
-        //  --------------------------
-
-        //  newIndex = Math.floor(Math.random() * 10) % img_array.length;
-        //  index = (newIndex === index) ? newIndex -1 : newIndex;
-
-        //  ------------------------------
-        //  For sequential image rotation:
-        //  ------------------------------
+        // newIndex = Math.floor(Math.random() * 10) % img_array.length;
+        // index = (newIndex === index) ? newIndex -1 : newIndex;
 
         index = (index + 1) % img_array.length;
 
         $('body').css('backgroundImage', function () {
-            $('#full-page').animate({
+            $('').animate({
                 backgroundColor: 'transparent'
-            }, 1000, function () {
+            }, 500, function () {                               //fade out
                 setTimeout(function () {
-                    $('#full-page').animate({
-                        backgroundColor: 'rgb(255,255,255)'
-                    }, 1000);
-                }, 3000);
+                    console.log("wtff");
+                    $('').animate({
+                        backgroundColor: 'rgb(0, 0, 0)'
+                    }, 500);                                    // fade in
+                }, 5000);
             });
             return 'url(https://raw.githubusercontent.com/kameoxylon/peterkoehler.co/master/images/image' + img_array[index] + '.jpg)';
         });
         setTimeout(changeBg, interval);
     })();
-});
+});*/
