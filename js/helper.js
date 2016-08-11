49eec098b67d203021fa2b6b88084384d1d328b5
kameoxylon <kre3ed@gmail.com>
2016-08-11 02:47:59 -0400

    final intro page

--- js/helper.js
+++ js/helper.js
@@ -1,28 +1,9 @@
-/*$(document).ready(function () {
-    var img_array = [0, 1, 2, 3, 4],
-        newIndex = 0,
-        index = 0,
-        interval = 6000;
-    (function changeBg() {
-
-        // newIndex = Math.floor(Math.random() * 10) % img_array.length;
-        // index = (newIndex === index) ? newIndex -1 : newIndex;
-
-        index = (index + 1) % img_array.length;
-
-        $('body').css('backgroundImage', function () {
-            $('').animate({
-                backgroundColor: 'transparent'
-            }, 500, function () {                               //fade out
-                setTimeout(function () {
-                    console.log("wtff");
-                    $('').animate({
-                        backgroundColor: 'rgb(0, 0, 0)'
-                    }, 500);                                    // fade in
-                }, 5000);
-            });
-            return 'url(https://raw.githubusercontent.com/kameoxylon/peterkoehler.co/master/images/image' + img_array[index] + '.jpg)';
-        });
-        setTimeout(changeBg, interval);
-    })();
-});*/
+$(document).ready(function(){
+    $("p").mouseover(function(){
+        $("p").css("background-color", "white");
+    });
+    $("p").mouseout(function(){
+        $("p").css("background-color", "");
+        $("p").css("color", "black");
+    });
+});
