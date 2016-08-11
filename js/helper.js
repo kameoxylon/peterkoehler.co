3c05fc7c95f482c6d7bcd479dc496f48833e338a
kameoxylon <kre3ed@gmail.com>
2016-08-11 03:10:19 -0400

    helper tweak

--- js/helper.js
+++ js/helper.js
@@ -1,9 +1,14 @@
-$(document).ready(function(){
-    $("p").mouseover(function(){
+$(document).ready(function () {
+    $("p").mouseover(function () {
         $("p").css("background-color", "white");
-    });
-    $("p").mouseout(function(){
-        $("p").css("background-color", "");
         $("p").css("color", "black");
+        $("p").css("opacity", ".80");
+        $("p").css("border-color", "white");
+    });
+    $("p").mouseout(function () {
+        $("p").css("background-color", "");
+        $("p").css("color", "white");
+        $("p").css("opacity", "1");
+        $("p").css("border-color", "white");
     });
 });
