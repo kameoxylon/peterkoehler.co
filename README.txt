4b322fe45eca7209eac847c8832cf643359c2d22
kameoxylon <kre3ed@gmail.com>
2016-08-12 01:27:43 -0400

    new page

--- README.txt
+++ README.txt
@@ -7,3 +7,5 @@ peterkoehler.co
 	- neon light image first
 https://www.flickr.com/photos/130005110@N05/shares/9X9n04
 https://www.flickr.com/photos/130005110@N05/shares/Mt6G51
+- 5 pages
+- odd person
--- js/helper.js
+++ js/helper.js
@@ -1,14 +1,45 @@
 $(document).ready(function () {
-    $("p").mouseover(function () {
-        $("p").css("background-color", "white");
-        $("p").css("color", "black");
-        $("p").css("opacity", ".80");
-        $("p").css("border-color", "white");
+    $(".intro-button-hover").mouseover(function () {
+        $(".intro-button-hover").css("background-color", "white");
+        $(".intro-button-hover").css("color", "black");
+        $(".intro-button-hover").css("opacity", ".80");
+        $(".intro-button-hover").css("border-color", "white");
     });
-    $("p").mouseout(function () {
-        $("p").css("background-color", "");
-        $("p").css("color", "white");
-        $("p").css("opacity", "1");
-        $("p").css("border-color", "white");
+    $(".intro-button-hover").mouseout(function () {
+        $(".intro-button-hover").css("background-color", "");
+        $(".intro-button-hover").css("color", "white");
+        $(".intro-button-hover").css("opacity", "1");
+        $(".intro-button-hover").css("border-color", "white");
     });
 });
+(function ($) {
+    $(document).ready(function () {
+
+        // hide .navbar first
+        $("#nav-bar").hide();
+        
+
+        
+
+        // fade in .navbar
+        $(function () {
+            $(window).scroll(function () {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 1) {
+                    $('#nav-bar').fadeIn();
+                    $('#name-nav-bar').fadeOut();
+                    $(".home-image-button1").parent().fadeOut();
+                    $(".home-image-button2").parent().fadeOut();
+                } else {
+                    $('#nav-bar').fadeOut();
+                    $('#name-nav-bar').fadeIn();
+                    $(".home-image-button1").parent().fadeIn();
+                    $(".home-image-button2").parent().fadeIn();
+                }
+            });
+
+
+        });
+
+    });
+}(jQuery));
