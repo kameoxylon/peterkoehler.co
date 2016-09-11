5bd8c999bde95fdc4603fa37a98ee6f1227b4160
kameoxylon <kre3ed@gmail.com>
2016-09-10 21:59:11 -0400

    update: readme

--- README.txt
+++ README.txt
@@ -1,11 +1,9 @@
 peterkoehler.co
 
 - contact page
-- rates/services page
 - prints page
 - enter page with 5 image sliders
 	- neon light image first
 https://www.flickr.com/photos/130005110@N05/shares/9X9n04
 https://www.flickr.com/photos/130005110@N05/shares/Mt6G51
-- 5 pages
-- odd person
+
--- js/helper.js
+++ js/helper.js
@@ -1,24 +1,8 @@
-$(document).ready(function () {
-    $(".intro-button-hover").mouseover(function () {
-        $(".intro-button-hover").css("background-color", "white");
-        $(".intro-button-hover").css("color", "black");
-        $(".intro-button-hover").css("opacity", ".80");
-        $(".intro-button-hover").css("border-color", "white");
-    });
-    $(".intro-button-hover").mouseout(function () {
-        $(".intro-button-hover").css("background-color", "");
-        $(".intro-button-hover").css("color", "white");
-        $(".intro-button-hover").css("opacity", "1");
-        $(".intro-button-hover").css("border-color", "white");
-    });
-});
 (function ($) {
     $(document).ready(function () {
 
         // hide .navbar first
         $("#nav-bar").hide();
-        
-
         
 
         // fade in .navbar
@@ -28,12 +12,12 @@ $(document).ready(function () {
                 if ($(this).scrollTop() > 1) {
                     $('#nav-bar').fadeIn();
                     $('#name-nav-bar').fadeOut();
-                    $(".home-image-button1").parent().fadeOut();
+                   // $(".home-image-button1").parent().fadeOut();
                     $(".home-image-button2").parent().fadeOut();
                 } else {
                     $('#nav-bar').fadeOut();
                     $('#name-nav-bar').fadeIn();
-                    $(".home-image-button1").parent().fadeIn();
+                   // $(".home-image-button1").parent().fadeIn();
                     $(".home-image-button2").parent().fadeIn();
                 }
             });
